561ec951bb53804e219f86600778d16f3f0d159b
Xiaofeng Bai <bxf12315@gmail.com>
2025-02-21 14:41:22 +0800

    new relationship entity for package and license

---
+++ migration/src/m0000030_create_extracted_licensing_infos.rs
@@ -0,0 +1,64 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(ExtractedLicensingInfos::Table)
+                    .col(
+                        ColumnDef::new(ExtractedLicensingInfos::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(ExtractedLicensingInfos::Name)
+                            .string()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(ExtractedLicensingInfos::LicenseId)
+                            .string()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(ExtractedLicensingInfos::SbomId)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(ExtractedLicensingInfos::ExtractedText).string())
+                    .col(ColumnDef::new(ExtractedLicensingInfos::Comment).string())
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(
+                Table::drop()
+                    .table(ExtractedLicensingInfos::Table)
+                    .to_owned(),
+            )
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+pub enum ExtractedLicensingInfos {
+    Table,
+    Id,
+    SbomId,
+    Name,
+    LicenseId,
+    ExtractedText,
+    Comment,
+}
