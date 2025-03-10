e00af34463a6d80f59c0fd4950571e970c7a4c40
mrizzi <mrizzi@redhat.com>
2025-03-10 17:48:10 +0100

    Refactor sbom_name, sbom_namespace, package_name
    Signed-off-by: mrizzi <mrizzi@redhat.com>

--- modules/fundamental/src/license/model/sbom_license.rs
+++ modules/fundamental/src/license/model/sbom_license.rs
@@ -4,9 +4,6 @@ use uuid::Uuid;
 
 #[derive(Debug, Clone, Default)]
 pub struct SbomPackageLicense {
-    pub sbom_namespace: Option<String>,
-    /// Package name
-    pub name: String,
     /// package package URL
     pub purl: Vec<Purl>,
     pub other_reference: Vec<trustify_entity::cpe::Model>,
@@ -28,16 +25,14 @@ pub struct Purl {
 
 #[derive(Debug, Clone, FromQueryResult)]
 pub struct SbomPackageLicenseBase {
-    pub sbom_name: Option<String>,
-    pub sbom_namespace: Option<String>,
     pub node_id: Option<String>,
     pub sbom_id: Uuid,
-    pub package_name: String,
     pub license_text: Option<String>,
 }
 
 #[derive(Debug, Clone, Default, FromQueryResult)]
 pub struct SbomNameGroupVersion {
+    pub sbom_namespace: String,
     pub sbom_name: String,
     pub sbom_group: Option<String>,
     pub sbom_version: String,
