22b47b9e3e17f27232f70813d6624ab4da1ad3c7
Xiaofeng Bai <bxf12315@gmail.com>
2025-05-21 13:28:27 +0800

    TC-1902 Merge two queries.

--- modules/fundamental/src/license/service/mod.rs
+++ modules/fundamental/src/license/service/mod.rs
@@ -7,7 +7,10 @@ use crate::{
         },
     },
 };
-use sea_orm::{ColumnTrait, ConnectionTrait, EntityTrait, QueryFilter, QuerySelect, RelationTrait};
+use sea_orm::{
+    ColumnTrait, ConnectionTrait, DbErr, EntityTrait, QueryFilter, QuerySelect, RelationTrait,
+    Statement,
+};
 use sea_query::{Condition, JoinType};
 use std::collections::HashSet;
 use trustify_common::{
@@ -19,7 +22,6 @@ use trustify_entity::{
     license, licensing_infos, qualified_purl, sbom, sbom_node, sbom_package, sbom_package_cpe_ref,
     sbom_package_license, sbom_package_purl_ref,
 };
-use uuid::Uuid;
 
 pub mod license_export;
 
@@ -209,51 +211,44 @@ impl LicenseService {
         Ok(None)
     }
 
-    pub async fn get_unique_licenses<C: ConnectionTrait>(
+    pub async fn get_all_license_info<C: ConnectionTrait>(
         &self,
-        sbom_id: Uuid,
+        sbom_id: Id,
         connection: &C,
     ) -> Result<HashSet<String>, Error> {
-        let result_package_licenses: Vec<Option<Vec<String>>> =
-            sbom_package_license::Entity::find()
-                .filter(sbom_package_license::Column::SbomId.eq(sbom_id))
-                .join(
-                    JoinType::InnerJoin,
-                    sbom_package_license::Relation::License.def(),
-                )
-                .select_only()
-                .column_as(license::Column::SpdxLicenses, "license_ids")
-                .into_tuple::<(Option<Vec<String>>,)>()
-                .all(connection)
+        if let Some(uuid) = sbom_id.try_as_uid() {
+            let stmt = Statement::from_sql_and_values(
+                connection.get_database_backend(),
+                r#"
+        (
+            SELECT DISTINCT unnest(l.spdx_licenses) as license_id
+            FROM sbom_package_license spl
+            JOIN license l ON spl.license_id = l.id
+            WHERE spl.sbom_id = $1
+            AND l.spdx_licenses IS NOT NULL
+        )
+        UNION
+        (
+            SELECT DISTINCT license_id
+            FROM licensing_infos
+            WHERE sbom_id = $1
+        )
+        "#,
+                [uuid.into()],
+            );
+
+            let result: Vec<String> = connection
+                .query_all(stmt)
                 .await?
                 .into_iter()
-                .map(|(ids,)| ids)
-                .collect();
+                .map(|row| row.try_get_by_index::<String>(0))
+                .collect::<Result<Vec<String>, DbErr>>()?;
 
-        let unique_licenses: HashSet<String> = result_package_licenses
-            .into_iter()
-            .flatten()
-            .flatten()
-            .collect();
+            return Ok(result.into_iter().collect());
+        }
 
-        Ok(unique_licenses)
-    }
-
-    pub async fn get_license_info<C: ConnectionTrait>(
-        &self,
-        sbom_id: Uuid,
-        connection: &C,
-    ) -> Result<HashSet<String>, Error> {
-        let result_license_infos: Vec<String> = licensing_infos::Entity::find()
-            .filter(licensing_infos::Column::SbomId.eq(sbom_id))
-            .select_only()
-            .column_as(licensing_infos::Column::LicenseId, "license_id")
-            .into_tuple::<(String,)>()
-            .all(connection)
-            .await?
-            .into_iter()
-            .map(|(license_id,)| license_id)
-            .collect();
-        Ok(result_license_infos.into_iter().collect())
+        Err(Error::Internal(
+            format!("Invalid UUID format. {:?}", sbom_id.clone()).to_string(),
+        ))
     }
 }
--- modules/fundamental/src/sbom/endpoints/mod.rs
+++ modules/fundamental/src/sbom/endpoints/mod.rs
@@ -5,7 +5,6 @@ mod query;
 mod test;
 
 pub use query::*;
-use std::collections::HashSet;
 
 use crate::{
     Error::{self, Internal},
@@ -85,10 +84,10 @@ const CONTENT_TYPE_GZIP: &str = "application/gzip";
     ),
     responses(
     (status = 200, description = "fetch all unique license id and license info id", body = Vec<String>),
-    (status = 404, description = "not found"),
+    (status = 400, description = "Invalid UUID format."),
     ),
 )]
-#[get("/v2/sbom/{id}/licenseids")]
+#[get("/v2/sbom/{id}/license-ids")]
 pub async fn get_unique_licenses(
     fetcher: web::Data<LicenseService>,
     db: web::Data<Database>,
@@ -96,17 +95,8 @@ pub async fn get_unique_licenses(
     _: Require<ReadSbom>,
 ) -> actix_web::Result<impl Responder> {
     let id = Id::from_str(&id).map_err(Error::IdKey)?;
-    let sbom_id = if let Some(uuid) = id.try_as_uid() {
-        uuid
-    } else {
-        Uuid::new_v4()
-    };
-    let spdx_licenses = fetcher.get_unique_licenses(sbom_id, db.as_ref()).await?;
-    let license_infos = fetcher.get_license_info(sbom_id, db.as_ref()).await?;
-    let mut result = HashSet::new();
-    result.extend(spdx_licenses);
-    result.extend(license_infos);
-    Ok(HttpResponse::Ok().json(result))
+
+    Ok(HttpResponse::Ok().json(fetcher.get_all_license_info(id, db.as_ref()).await?))
 }
 
 #[utoipa::path(
